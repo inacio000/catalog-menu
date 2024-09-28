c33e179a8974feec0896182c9e6ec3b21b1f7047
Inácio Raimundo <84747204+inacio000@users.noreply.github.com>
2024-09-29 02:18:05 +0300

    feat: add new Icon components

---
+++ src/assets/components/arrowLeftIcon.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function ArrowLeftIcon() {
+    return (
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M10.3998 12L14.9998 7.4L13.5998 6L7.59982 12L13.5998 18L14.9998 16.6L10.3998 12Z" fill="#7A7F89" />
+        </svg>
+
+    )
+}
---
+++ src/assets/components/arrowRightIcon.tsx
@@ -0,0 +1,8 @@
+export default function ArrowRightIcon() {
+    return (
+        <svg width="50" height="25" viewBox="0 0 8 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M4.60018 6L0.000183044 10.6L1.40018 12L7.40018 6L1.40018 -6.11959e-08L0.000182642 1.4L4.60018 6Z" fill="#959BAD" />
+        </svg>
+
+    )
+}
