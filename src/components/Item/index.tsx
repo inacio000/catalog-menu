941b6c863b3b4095cc6a0e0bc8005ea13f4f13e9
Inácio Raimundo <84747204+inacio000@users.noreply.github.com>
2024-09-29 02:14:41 +0300

    refactor: refactor Item component

--- src/components/Item/index.tsx
+++ src/components/Item/index.tsx
@@ -1,38 +1,46 @@
-'use client'
-
-import styles from "./styles.module.scss"
-import { usePathname, useRouter } from "next/navigation"
-import { useMemo } from "react"
+// SidebarItem.tsx
+import React, { useMemo } from 'react';
+import { useRouter, usePathname } from 'next/navigation';
+import styles from './styles.module.scss';
+import ArrowRightIcon from '@/assets/components/arrowRightIcon';
 
 export interface SidebarItemsProps {
-  name: string
-  path: string
+  name: string;
+  path: string;
 }
 
-export const SidebarItem = ({ item, setLastPath }: { item: SidebarItemsProps, setLastPath: (path: string) => void }) => {
-  const { name, path } = item
-  const router = useRouter()
-  const pathName = usePathname()
+export const SidebarItem = ({
+  item,
+  setLastPath,
+  onClick
+}: {
+  item: SidebarItemsProps;
+  setLastPath: (path: string) => void
+  onClick?: () => void;
+}) => {
+  const { name, path } = item;
+  const router = useRouter();
+  const pathName = usePathname();
+
+  const isMobile = typeof window !== 'undefined' && window.innerWidth <= 767;
 
   const handlePage = () => {
-    router.push(path)
-    setLastPath(path)
-  }
+    router.push(path);
+    setLastPath(path);
+    onClick
+  };
 
-  const isActive = useMemo(() => {
-    return path === pathName
-  }, [path, pathName])
+  const isActive = useMemo(() => path === pathName, [path, pathName]);
 
   return (
-    <>
-      <nav
-        className={`${styles.sidebar_content}`}
-        onMouseEnter={handlePage}
-      >
-        <p className={`${isActive && `${styles.sidebar_active}`}`}>
-          {name}
-        </p>
-      </nav>
-    </>
-  )
-}
+    <nav
+      className={`${styles.sidebar_content} ${isActive && styles.sidebar_active}`}
+      onClick={isMobile ? handlePage : undefined}
+      onMouseEnter={!isMobile ? handlePage : undefined}
+    >
+      <div className={styles.item}>
+        <span>{name}</span>
+      </div>
+    </nav>
+  );
+};
